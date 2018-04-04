7f4b0f0508313ed15a49afab59c78897da5c2430
Andrew White <andrew.white@rochester.edu>
2018-04-04 09:22:27 -0400

    Added selection for peptide visualization

--- client/components/lib/sequence.js
+++ client/components/lib/sequence.js
@@ -1,9 +1,10 @@
-import {event, select} from 'd3-selection'
+import {event, select, mouse} from 'd3-selection'
 import {forceSimulation, forceManyBody, forceCenter, forceLink, forceX, forceY} from 'd3-force'
 import {drag} from 'd3-drag'
 
 class SequenceD3 {
   constructor (canvas) {
+    this.canvas = canvas
     this.context = canvas.getContext('2d')
     this.width = canvas.width
     this.height = canvas.height
@@ -33,6 +34,28 @@ class SequenceD3 {
           event.subject.fx = null
           event.subject.fy = null
         }))
+    select(canvas).on('mouseenter', () => {
+      // when we are hovered, do this
+      select(canvas).on('mousemove', () => {
+        let xy = mouse(this.canvas)
+        let subject = this.simulation.find(xy[0], xy[1])
+        // console.log(xy)
+        if (subject) {
+          this.selected = subject.id
+          this.ticked() // call tick to make sure redrawn
+        } else
+          this.selected = -1
+      })
+    })
+      .on('mouseleave', () => {
+        select(canvas).on('mousemove', null)
+      })
+
+    // default size
+    this.radius = 250
+    this.context.textAlign = 'center'
+    this.context.textBaseline = 'middle'
+    this.selected = -1
 
     this.simulation.on('tick', this.ticked.bind(this))
     this.update('')
@@ -51,6 +74,10 @@ class SequenceD3 {
       this.simulation.force('x').strength(Math.min(0.05, this.nodes.length * 0.05 / 50))
       this.simulation.force('y').strength(Math.min(0.1, this.nodes.length * 0.1 / 50))
     }
+
+    // adjust drawing properties
+    this.radius = Math.max(5, 250 / (this.nodes.length + 1))
+    this.context.font = `${Math.round(Math.max(10, 72 / Math.sqrt(1 + this.nodes.length)))}px sans-serif`
 
     this.simulation.nodes(this.nodes)
     this.links = this.simulation.nodes().slice(0, -1).map((n, i) => { return {source: i, target: i + 1} })
@@ -72,12 +99,30 @@ class SequenceD3 {
 
     this.context.beginPath()
     this.nodes.forEach((d) => {
-      this.context.moveTo(d.x + 3, d.y)
-      this.context.arc(d.x, d.y, 3, 0, 2 * Math.PI)
+      this.context.moveTo(d.x + this.radius, d.y)
+      this.context.arc(d.x, d.y, this.radius, 0, 2 * Math.PI)
     })
+    // remove lines inside the cirlce
+    this.context.fillStyle = '#FFF'
     this.context.fill()
-    this.context.strokeStyle = '#fff'
+    this.context.strokeStyle = '#666'
     this.context.stroke()
+
+    // hanlde selection
+    if (this.selected >= 0) {
+      this.context.beginPath()
+      this.context.fillStyle = '#FF3434'
+      this.context.moveTo(this.nodes[this.selected] + this.radius, this.nodes[this.selected].y)
+      this.context.arc(this.nodes[this.selected].x, this.nodes[this.selected].y, this.radius, 0, 2 * Math.PI)
+      this.context.fill()
+      this.context.strokeStyle = '#666'
+      this.context.stroke()
+    }
+
+    this.context.fillStyle = '#444'
+    this.nodes.forEach((d) => {
+      this.context.fillText(d.name.toUpperCase(), d.x, d.y)
+    })
   }
 }
 
