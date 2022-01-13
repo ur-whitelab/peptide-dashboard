e6b323cb0dc4ff69b046b3179c461351c13eef71
Andrew White <white.d.andrew@gmail.com>
2022-01-13 16:05:11 -0500

    Trying new version of TFJS

--- src/app.js
+++ src/app.js
@@ -1,12 +1,3 @@
-// not sure what the fuck is going on
-// but TFJS is shitting the bed with fetch
-import fetch from 'node-fetch';
-
-globalThis.fetch = fetch;
-globalThis.Headers = Headers;
-globalThis.Request = Request;
-globalThis.Response = Response;
-
 import { createApp } from "vue";
 import App from './components/App.vue'
 const app = createApp(App);
