676b6f26cfc4e67f86fdc0ab0859a2c57efe92ba
Andrew White <white.d.andrew@gmail.com>
2022-01-13 15:19:40 -0500

    Removed logging code

--- src/components/SequenceViewer.vue
+++ src/components/SequenceViewer.vue
@@ -3,52 +3,49 @@
 </template>
 
 <script>
-import SequenceD3 from './lib/sequence.js'
-import _ from 'lodash'
+import SequenceD3 from "./lib/sequence.js";
+import _ from "lodash";
 export default {
-  name: 'SequenceViewer',
+  name: "SequenceViewer",
   props: {
     sequence: String,
-    viewWidth: Number
+    viewWidth: Number,
   },
-  data () {
+  data() {
     return {
-      sd3: null
-    }
+      sd3: null,
+    };
   },
   mounted: function () {
-    this.updateCanvasSize()
+    this.updateCanvasSize();
   },
   watch: {
     sequence: _.debounce(function () {
-      this.sd3.update(this.sequence)
+      this.sd3.update(this.sequence);
     }, 500),
 
     viewWidth: function () {
-      this.updateCanvasSize()
-    }
-
+      this.updateCanvasSize();
+    },
   },
   methods: {
     updateCanvasSize: function () {
       if (this.$refs.sequenceGraph.width !== this.viewWidth) {
-        this.$refs.sequenceGraph.width = this.viewWidth
-        let ctx = this.$refs.sequenceGraph.getContext('2d')
-        console.log('updated: ' + this.viewWidth + ' now ' + this.$refs.sequenceGraph.width + ' canvas ' + ctx.canvas.width)
-        ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
+        this.$refs.sequenceGraph.width = this.viewWidth;
+        let ctx = this.$refs.sequenceGraph.getContext("2d");
+        ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
         if (!this.sd3)
-          this.sd3 = new SequenceD3(this.$refs.sequenceGraph, (index) => { this.$emit('selection-update', index) })
-        else
-          this.sd3.width = this.viewWidth
+          this.sd3 = new SequenceD3(this.$refs.sequenceGraph, (index) => {
+            this.$emit("selection-update", index);
+          });
+        else this.sd3.width = this.viewWidth;
 
-        this.sd3.update(this.sequence)
+        this.sd3.update(this.sequence);
       }
-    }
-  }
-}
+    },
+  },
+};
 </script>
 
 <style lang="scss">
-
-
 </style>
