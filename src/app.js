a95e01b1d1058fb34fe3ee3f8287e30285244758
Andrew White <white.d.andrew@gmail.com>
2022-01-13 15:58:15 -0500

    Put Fetch polyfill?WTF?

--- src/app.js
+++ src/app.js
@@ -1,3 +1,12 @@
+// not sure what the fuck is going on
+// but TFJS is shitting the bed with fetch
+import fetch from 'node-fetch';
+
+globalThis.fetch = fetch;
+globalThis.Headers = Headers;
+globalThis.Request = Request;
+globalThis.Response = Response;
+
 import { createApp } from "vue";
 import App from './components/App.vue'
 const app = createApp(App);
