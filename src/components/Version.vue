a3a26f190a5a288dd54b2ebb3993a688e0027c5e
Andrew White <white.d.andrew@gmail.com>
2022-01-16 13:02:46 -0500

    Updated version text

--- src/components/Version.vue
+++ src/components/Version.vue
@@ -11,11 +11,11 @@
       </div>
       <div class="message-body has-text-dark">
         <strong> v{{ version }}</strong> You are viewing an early version of
-        ongoing research. The models and predictions shown here are based on
-        models that are incomplete. I have made this accessible to gather
-        feedback and engage other experts in peptide chemistry. Please come back
-        often for newer versions. See the source code, open an issue if see a
-        problem, and watch the
+        ongoing research. The predictions shown here are based on models that
+        are still being improved. I have made this accessible to gather feedback
+        and engage other experts in peptide chemistry. Please come back often
+        for newer versions. See the source code, open an issue if see a problem,
+        and watch the
         <a href="https://github.com/ur-whitelab/peptide-dashboard"
           >github repo</a
         >
